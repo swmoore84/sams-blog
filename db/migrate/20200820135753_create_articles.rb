5e46b79c6bc3d1ebbf0f6c40acc61f203bd9804c
Sam Moore <swmoore84@gmail.com>
2020-08-20 23:00:35 +0900

    created articles model and controller

---
+++ db/migrate/20200820135753_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.date :published_date
+      t.boolean :published
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
