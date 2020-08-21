18ff49b794fc44ff5810070ea1ef18f27e6ced8f
Sam Moore <swmoore84@gmail.com>
2020-08-21 12:02:34 +0900

    updated seed file to include user_id

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
-  validates :published_date, presence: true
+  # validates :published_date, presence: true
 end
