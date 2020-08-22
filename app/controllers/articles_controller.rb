fda695d3e359782b3dfc184785865dc6897df69e
Sam Moore <swmoore84@gmail.com>
2020-08-22 16:16:13 +0900

    created search function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,11 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    if params[:query].present?
+      @articles = Article.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @articles = Article.all
+    end
   end
 
   def show
