fc86c7f5c5b126dd20b7e937152c53ee1911093b
Sam Moore <swmoore84@gmail.com>
2020-08-24 15:21:47 +0900

    updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
+3.times do |user|
+  User.create!(
+    email: "test#{user + 1}@test.com",
+    password: "testpassword"
+  )
+end
 
-
+puts "3 users created"
 
 
 10.times do |article|
   Article.create!(
     title: "My Blog Post #{article}",
+    published: true,
+    published_date: "2020-08-24",
     content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-    user_id: 1,
+    user_id: rand(1..3),
   )
 end
 
