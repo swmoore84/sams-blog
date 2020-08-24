2f666ff07e94cbb370e97f536d6de9b712db95c2
Sam Moore <swmoore84@gmail.com>
2020-08-24 15:35:00 +0900

    validating present of published date

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :content, presence: true
-  # validates :published_date, presence: true
+  validates :published_date, presence: true
 
 end
